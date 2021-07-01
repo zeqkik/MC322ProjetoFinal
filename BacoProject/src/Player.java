06b2d4a56c076acb738d582bacc48a099056cb31
Pedro077 <pdo_henrique@hotmail.com>
2021-06-30 23:05:35 -0300

    começo da classe Player, desenvolvimento do metodo takeCard, mas ta osso

--- BacoProject/src/Player.java
+++ BacoProject/src/Player.java
@@ -4,19 +4,26 @@ import java.util.Random;
 public class Player {
     int mana, nexus;
     Card[] deck = new Card[40];
-    ArrayList<Card> hands = new ArrayList<Card>();
+    Card[] hands = new Card[10];
+    //ArrayList<Card> hands = new ArrayList<Card>();
     TipeAttack tipe_attack;
 
     //pega do deck
-    public void takeCard(){
+    public void takeCard() {
         Random r = new Random();
 
-        if (numRound == 0){
+        if (numRound == 0) {
             for (int i = 0; i < 4; ++i)
-                hands.add(deck[r.nextInt()]);
+                hands[i] = deck[r.nextInt()];
         }
         else
-            hands.add(deck[r.nextInt()]);
+            for (int i = 0; i < 9; ++i) { //sei que ta errado mas to burro hj, amanhã vejo com calma
+                for (int j = 0; j < 36; ++j)
+                   if (hands[i] == null && deck[j] != null)
+                       hands[i] = deck[j];
+
+            break;
+            }
     }
 
     //invoca no campo
