5a4316b9b506156f632782b85a3295b39986dd19
Pedro077 <pdo_henrique@hotmail.com>
2021-06-30 21:03:31 -0300

    começo da classe Player (ver como fazer atack (enum, herança, boolean..)

--- BacoProject/src/Player.java
+++ BacoProject/src/Player.java
@@ -1,19 +1,27 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Player {
-    int mana;
-    Card deck;
-    boolean atack  = true;  //Se verdadeiro o player ataca, se não, se defende.
+    int mana, nexus;
+    Card deck[] = new Card[40];
+    ArrayList<Card> hands = new ArrayList<Card>();
+    boolean atack = true;  //Se verdadeiro o player ataca, se não, se defende. (Ver a melhor maneira de fazer)
 
+    //pega do deck
+    public void takeCard(){
+        Randon r = new Random();
 
-
-    public void buyCard(){
-
+        if (numRound == 0){
+            for (int i = 0; i < 4; ++i)
+                hands.add(deck[r.nextInt()]);
+        }
+        else
+            hands.add(deck[r.nextInt()]);
     }
 
-    public void changeCard(){
+    //invoca no campo
+    public void evoke(){
 
     }
-
-
-
 
 }
