84c51dd549a415f7d1cd78cfd5d63fb196a34bd7
Pedro077 <pdo_henrique@hotmail.com>
2021-07-09 15:22:30 -0300

    começo desenvolcimento metodo card

--- BacoProject/src/BacoProject/Player.java
+++ BacoProject/src/BacoProject/Player.java
@@ -2,14 +2,14 @@ package BacoProject;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Player {
     int mana;
     int nexusLife;
     ArrayList<Card> deck = new ArrayList<Card>();
     ArrayList<Card> hands = new ArrayList<Card>();
-    //ArrayList<Card> hands = new ArrayList<Card>();
-    TipeAttack tipe_attack;
+    boolean tipe_attack = true;
 
     public Player() {
         this.mana = 0;
@@ -29,13 +29,6 @@ public class Player {
                 hands.remove(j);
             }
         }
-
-
-    }
-
-    //invoca no campo
-    public void evoke() {
-
     }
 
     public void giveInitialCards() {
@@ -45,9 +38,28 @@ public class Player {
         }
     }
 
+    //invoca no campo
+    public Card evoke() {
+        Scanner card_hand = new Scanner(System.in);
+        System.out.println("Selecione a carta que deseja invocar");
+        //colcocar um print com as listas de cartas em mãos
+
+        int choose_card = card_hand.nextInt();
+        if (choose_card > hands.size()) {
+            System.out.println("Posicao invalida");
+        }
+
+
+
+
+        //escolhe uma carta da mão
+        //paga a mana
+        //coloca na board
+    }
+
     private void checkSizeDeck() throws SizeException {
         if (hands.size() > 10) {
-            throw new SizeException("Numero de cartas invaido");
+            throw new SizeException("Numero de cartas invalido");
         }
     }
 
